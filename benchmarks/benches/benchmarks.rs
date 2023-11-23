b439795f22f468e003ed57ed623cdbc9e7bcbb9a
Coda Hale <coda.hale@gmail.com>
2023-11-23 11:10:52 -0700

    perf(benchmarks): add throughput benchmark

--- benchmarks/benches/benchmarks.rs
+++ benchmarks/benches/benchmarks.rs
@@ -1,7 +1,8 @@
 #![allow(elided_lifetimes_in_paths)]
 
+use divan::counter::BytesCount;
 use rand::rngs::OsRng;
-use tokio::io;
+use tokio::io::{self, AsyncReadExt, BufReader};
 use yrgourd::{PrivateKey, Transport};
 
 #[divan::bench]
@@ -36,6 +37,54 @@ fn handshake(bencher: divan::Bencher) {
                         .await
                         .unwrap();
 
+                        t.shutdown().await.unwrap();
+                    });
+
+                    acceptor.await.unwrap();
+                    initiator.await.unwrap();
+                });
+            },
+        );
+}
+
+const KB: u64 = 1024;
+const LENS: &[u64] = &[0, KB, 8 * KB, 32 * KB, 64 * KB, 128 * KB, KB * KB];
+
+#[divan::bench(consts = LENS)]
+fn transfer<const LEN: u64>(bencher: divan::Bencher) {
+    bencher
+        .with_inputs(|| {
+            let rt = tokio::runtime::Builder::new_multi_thread().enable_all().build().unwrap();
+            let acceptor_key = PrivateKey::random(OsRng);
+            let acceptor_public_key = acceptor_key.public_key;
+            let initiator_key = PrivateKey::random(OsRng);
+            let (initiator, acceptor) = io::duplex(64);
+            (rt, acceptor_key, initiator_key, acceptor_public_key, initiator, acceptor)
+        })
+        .counter(BytesCount::new(LEN))
+        .bench_values(
+            |(rt, acceptor_key, initiator_key, acceptor_public_key, initiator, acceptor)| {
+                rt.block_on(async {
+                    let acceptor = tokio::spawn(async move {
+                        let mut t = Transport::accept_handshake(acceptor, OsRng, &acceptor_key)
+                            .await
+                            .unwrap();
+                        io::copy_buf(&mut t, &mut io::sink()).await.unwrap();
+                        t.shutdown().await.unwrap();
+                    });
+
+                    let initiator = tokio::spawn(async move {
+                        let mut t = Transport::initiate_handshake(
+                            initiator,
+                            OsRng,
+                            &initiator_key,
+                            acceptor_public_key,
+                        )
+                        .await
+                        .unwrap();
+                        io::copy_buf(&mut BufReader::new(io::repeat(0xed).take(LEN)), &mut t)
+                            .await
+                            .unwrap();
                         t.shutdown().await.unwrap();
                     });
 
