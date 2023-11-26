f7661ac6f007e895dd46510c60953525a716aba9
Coda Hale <coda.hale@gmail.com>
2023-11-26 10:54:27 -0700

    perf(benchmarks): remove more unwraps

--- benchmarks/benches/benchmarks.rs
+++ benchmarks/benches/benchmarks.rs
@@ -9,7 +9,10 @@ use yrgourd::{Acceptor, Initiator, PrivateKey};
 fn handshake(bencher: divan::Bencher) {
     bencher
         .with_inputs(|| {
-            let rt = tokio::runtime::Builder::new_multi_thread().enable_all().build().unwrap();
+            let rt = tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build()
+                .expect("should have tokio/rt-multi-thread enabled");
             let acceptor_key = PrivateKey::random(OsRng);
             let acceptor_pub = acceptor_key.public_key;
             let acceptor = Acceptor::new(OsRng, acceptor_key);
@@ -22,22 +25,19 @@ fn handshake(bencher: divan::Bencher) {
             |(rt, mut acceptor, mut initiator, acceptor_pub, initiator_conn, acceptor_conn)| {
                 rt.block_on(async {
                     let acceptor = tokio::spawn(async move {
-                        let t = acceptor.accept_handshake(acceptor_conn).await.unwrap();
-
-                        t.shutdown().await.unwrap();
+                        let t = acceptor.accept_handshake(acceptor_conn).await?;
+                        t.shutdown().await
                     });
 
                     let initiator = tokio::spawn(async move {
-                        let t = initiator
-                            .initiate_handshake(initiator_conn, acceptor_pub)
-                            .await
-                            .unwrap();
-                        t.shutdown().await.unwrap();
+                        let t = initiator.initiate_handshake(initiator_conn, acceptor_pub).await?;
+                        t.shutdown().await
                     });
 
-                    acceptor.await.unwrap();
-                    initiator.await.unwrap();
-                });
+                    acceptor.await??;
+                    initiator.await?
+                })
+                .expect("should handshake successfully");
             },
         );
 }
@@ -47,7 +47,10 @@ fn transfer(bencher: divan::Bencher) {
     const LEN: u64 = 100 * 1024 * 1024;
     bencher
         .with_inputs(|| {
-            let rt = tokio::runtime::Builder::new_multi_thread().enable_all().build().unwrap();
+            let rt = tokio::runtime::Builder::new_multi_thread()
+                .enable_all()
+                .build()
+                .expect("should have tokio/rt-multi-thread enabled");
             let acceptor_key = PrivateKey::random(OsRng);
             let acceptor_pub = acceptor_key.public_key;
             let acceptor = Acceptor::new(OsRng, acceptor_key);
@@ -61,23 +64,22 @@ fn transfer(bencher: divan::Bencher) {
             |(rt, mut acceptor, mut initiator, acceptor_pub, initiator_conn, acceptor_conn)| {
                 rt.block_on(async {
                     let acceptor = tokio::spawn(async move {
-                        let mut t = acceptor.accept_handshake(acceptor_conn).await.unwrap();
-                        io::copy(&mut t, &mut io::sink()).await.unwrap();
-                        t.shutdown().await.unwrap();
+                        let mut t = acceptor.accept_handshake(acceptor_conn).await?;
+                        io::copy(&mut t, &mut io::sink()).await?;
+                        t.shutdown().await
                     });
 
                     let initiator = tokio::spawn(async move {
-                        let mut t = initiator
-                            .initiate_handshake(initiator_conn, acceptor_pub)
-                            .await
-                            .unwrap();
-                        io::copy(&mut io::repeat(0xed).take(LEN), &mut t).await.unwrap();
-                        t.shutdown().await.unwrap();
+                        let mut t =
+                            initiator.initiate_handshake(initiator_conn, acceptor_pub).await?;
+                        io::copy(&mut io::repeat(0xed).take(LEN), &mut t).await?;
+                        t.shutdown().await
                     });
 
-                    acceptor.await.unwrap();
-                    initiator.await.unwrap();
-                });
+                    acceptor.await??;
+                    initiator.await?
+                })
+                .expect("should transfer successfully");
             },
         );
 }
